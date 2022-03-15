bcabb8579e4093f473b4b533097ee6295c67cd26
lizhuoli <lizhuoli@bytedance.com>
2022-03-15 19:36:15 +0800

    first commit

--- CGContextDrawImageBug/ViewController.m
+++ CGContextDrawImageBug/ViewController.m
@@ -15,7 +15,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *imagePath = [NSBundle.mainBundle pathForResource:@"TestImage" ofType:@"bmp"];
+    UIImage *image = [[UIImage alloc] initWithContentsOfFile:imagePath];
+    NSParameterAssert(image);
+    
+    // CGBitmapContextCreate BGRX8888 will result wrong black image
+    UIImage *image1 = [self testCGContextBugWithImage:image alpha:NO];
+    
+    // CGBitmapContextCreate BGRA8888 will result correct image
+    UIImage *image2 = [self testCGContextBugWithImage:image alpha:YES];
+    
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:image1];
+    [self.view addSubview:imageView1];
+    imageView1.frame = CGRectMake(0, 0, 200, 200);
+    
+    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:image2];
+    [self.view addSubview:imageView2];
+    imageView2.frame = CGRectMake(0, 300, 200, 200);
+}
+
+- (UIImage *)testCGContextBugWithImage:(UIImage *)image alpha:(BOOL)alpha {
+    CGImageRef cgimage = image.CGImage;
+    NSParameterAssert(cgimage);
+    
+    CGFloat width = CGImageGetWidth(cgimage);
+    CGFloat height = CGImageGetHeight(cgimage);
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGBitmapInfo bitmapInfo = kCGBitmapByteOrder32Little;
+    if (alpha) {
+        // BGRA
+        bitmapInfo |= kCGImageAlphaPremultipliedFirst;
+    } else {
+        // BGRX
+        bitmapInfo |= kCGImageAlphaNoneSkipFirst;
+    }
+    
+    CGContextRef context = CGBitmapContextCreate(NULL, width, height, 8, 0, colorspace, bitmapInfo);
+    NSParameterAssert(context);
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), cgimage);
+    CGImageRef createdCGImage = CGBitmapContextCreateImage(context);
+    
+    UIImage *result = [UIImage imageWithCGImage:createdCGImage];
+    CGImageRelease(createdCGImage);
+    
+    return result;
 }
 
 
